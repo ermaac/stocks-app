8c604faeadcfd3c26a9015ead6fb992960eab606
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2024-11-16 14:05:08 +0100

    DB: create marketplace_users

---
+++ db/migrate/20241116130331_create_marketplace_users.rb
@@ -0,0 +1,10 @@
+class CreateMarketplaceUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :marketplace_users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_16_113341) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_16_130331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "marketplace_users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
