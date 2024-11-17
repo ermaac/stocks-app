7165fb2ad8941c83fb90e868eabe5d5314b099a2
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2024-11-17 12:22:00 +0100

    Marketplace: block shares amount when creating a share order

--- app/services/marketplace/actions/create_share_order.rb
+++ app/services/marketplace/actions/create_share_order.rb
@@ -1,15 +1,29 @@
 module Marketplace
   module Actions
     class CreateShareOrder < Actor
-      input :shares_amount
-      input :price_per_share
+      input :shares_amount, type: Integer
+      input :price_per_share, type: Integer
       input :organization, type: Organization
       input :user, type: Marketplace::User
 
       output :share_order
 
       def call
-        self.share_order = Marketplace::ShareOrder.create!(
+        ActiveRecord::Base.transaction do
+          block_shares_amount
+          self.share_order = create_share_order
+        end
+      end
+
+      private
+
+      def block_shares_amount
+        organization.increment!(:blocked_shares_amount, shares_amount)
+        organization.decrement!(:available_shares_amount, shares_amount)
+      end
+
+      def create_share_order
+        Marketplace::ShareOrder.create!(
           shares_amount: shares_amount,
           price_per_share: price_per_share,
           organization: organization,
