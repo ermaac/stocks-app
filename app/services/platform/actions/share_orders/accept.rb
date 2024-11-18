2059b83101cbb3f5860637115bc62a2adb18b5d9
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2024-11-18 18:37:57 +0100

    Platform: check a order state before accepting or rejecting

--- app/services/platform/actions/share_orders/accept.rb
+++ app/services/platform/actions/share_orders/accept.rb
@@ -2,10 +2,22 @@ module Platform
   module Actions
     module ShareOrders
       class Accept < Actor
-        input :share_order, type: Marketplace::ShareOrder
+        input :share_order, type: Marketplace::ShareOrder, allow_nil: ServiceActorArguments::FORBID_NIL_VALUES
         input :user, type: Platform::User
 
         def call
+          fail!(error: "Can not accept order in #{share_order.state} state") unless can_accept?
+
+          accept!
+        end
+
+        private
+
+        def can_accept?
+          share_order.pending?
+        end
+
+        def accept!
           share_order.update!(
             state: :accepted,
             modified_by: user
--- app/services/platform/actions/share_orders/reject.rb
+++ app/services/platform/actions/share_orders/reject.rb
@@ -6,6 +6,18 @@ module Platform
         input :user, type: Platform::User
 
         def call
+          fail!(error: "Can not reject order in #{share_order.state} state") unless can_reject?
+
+          reject!
+        end
+
+        private
+
+        def can_reject?
+          share_order.registered?
+        end
+
+        def reject!
           share_order.update!(
             state: :rejected,
             modified_by: user
