5a661f52ae7e16870fe98e2c4b8e15508a74cdf0
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2024-11-19 21:50:40 +0100

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-3.times do
-  total_shares_amount = (rand(5) + 1) * 100_000_000
-  Organization.create(
-    name: Faker::Company.unique.name,
-    total_shares_amount: total_shares_amount,
-    available_shares_amount: rand * total_shares_amount
+
+def create_platform_user(organization, user_params = {})
+  organization.users.create!(
+    email: user_params.fetch(:email, Faker::Internet.unique.email),
+    password: user_params.fetch(:password, Faker::Internet.password),
+    first_name: user_params.fetch(:first_name, Faker::Name.first_name),
+    last_name: user_params.fetch(:last_name, Faker::Name.last_name),
+    role: user_params.fetch(:role, Platform::User::ROLES.first)
   )
+end
+
+def create_marketplace_user(user_params = {})
+  Marketplace::User.create!(
+    email: user_params.fetch(:email, Faker::Internet.unique.email),
+    username: user_params.fetch(:username, Faker::Internet.username),
+    password: user_params.fetch(:password, Faker::Internet.password)
+  )
+end
+
+def create_organization(org_params = {})
+  Organization.create!(
+    name: org_params.fetch(:name, Faker::Company.unique.name),
+    total_shares_amount: org_params.fetch(:total_shares_amount, Faker::Number.between(from: 10_000, to: 1_000_000)),
+    available_shares_amount: org_params.fetch(:available_shares_amount, Faker::Number.between(from: 1_000, to: 10_000))
+  )
+end
+
+3.times do |i|
+  organization = create_organization
+  create_platform_user(organization)
+  create_marketplace_user
 end
 2.times do
-  total_shares_amount = (rand(5) + 1) * 100_000_000
-  Organization.create(
-    name: Faker::Company.unique.name,
-    total_shares_amount: total_shares_amount,
-    available_shares_amount: 0
-  )
+  organization = create_organization(available_shares_amount: 0)
+  create_platform_user(organization)
+  create_marketplace_user
 end
+
+sample_organization = Organization.take
+create_platform_user(sample_organization,  email: 'businessowner@test.com', password: 'password123')
+create_marketplace_user(username: 'samplebuyer', password: 'password456')
